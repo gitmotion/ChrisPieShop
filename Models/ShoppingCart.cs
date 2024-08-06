bfe026b29950a2a3830ce18f7782ffd21673c41e
chadmin <chrishancode@gmail.com>
2024-08-05 18:09:18 -0700

    Added Shopping cart functionality

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChrisPieShop.Models
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        private readonly ChrisPieShopDbContext _chrisPieShopDbContext;
+
+        public string? ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
+
+        private ShoppingCart(ChrisPieShopDbContext chrisPieShopDbContext)
+        {
+            _chrisPieShopDbContext = chrisPieShopDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+            ChrisPieShopDbContext context = services.GetService<ChrisPieShopDbContext>() ?? throw new Exception("Error initializing");
+
+            string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session?.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _chrisPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Pie = pie,
+                    Amount = 1
+                };
+
+                _chrisPieShopDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _chrisPieShopDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _chrisPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _chrisPieShopDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _chrisPieShopDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??=
+                       _chrisPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                           .Include(s => s.Pie)
+                           .ToList();
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _chrisPieShopDbContext
+                .ShoppingCartItems
+                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+
+            _chrisPieShopDbContext.ShoppingCartItems.RemoveRange(cartItems);
+
+            _chrisPieShopDbContext.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _chrisPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Pie.Price * c.Amount).Sum();
+            return total;
+        }
+    }
+}
